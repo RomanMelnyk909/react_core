f438b6df245e09d4988d0b1e2a2170596bf08c29
RomanMelnyk909 <pvrrivne@gmail.com>
2023-11-09 19:23:44 +0200

    lists_rendering

--- my_app/src/components/Header/Header.jsx
+++ my_app/src/components/Header/Header.jsx
@@ -1,19 +1,49 @@
-import NavigationItem from'../NavigationItem'
+import NavigationItem from '../NavigationItem/NavigationItem';
 import './header.css';
 
 
 const Header = () => {
+
+   const navElements = [
+      {
+         text: 'First',
+         isUppercasetext: true,
+         description: "second description",
+      },
+      {
+         text: 'Second',
+         isUppercasetext: true,
+         description: "second description",
+      },
+      {
+         text: 'Third',
+         isUppercasetext: true,
+         description: "second description",
+      },
+      {
+         text: 'samsung',
+         isUppercasetext: true,
+      },
+   ];
+
+  
+
    return (
       <header>
       <div className='header'>
-         <NavigationItem text='main' isUppercasetext={true} />
-         <NavigationItem text='navigation' isUppercasetext={true} />
-         <NavigationItem text='menu' isUppercasetext={true} />
-         <NavigationItem text='etc' isUppercasetext={true} />
+      {
+            navElements.map((element) => {
+            return (
+               <NavigationItem
+                  text={element.text}
+                  isUppercasetext={element.isUppercasetext}
+                  description={element.description} />
+            )
+         })
+         }
       </div>
       <NavigationItem text='header' isUppercasetext={false} />
       </header>);
-
 }
 
-export default Header
+export default Header;
--- my_app/src/components/navigationItem/NavigationItem.jsx
+++ my_app/src/components/navigationItem/NavigationItem.jsx
@@ -1,13 +1,22 @@
+import './NavigationItem.css';
+
 const NavigationItem = (props) => {
-   const { text, isUppercasetext } = props
-   let textToRender;
-   isUppercasetext ? textToRender = text.toUpperCase() : textToRender = text;
+   const { text, isUppercasetext, description } = props
+
+   const descriptionSection = (
+         <div className='description'>
+             { description }
+         </div>
+   );
+  
    return (
-      <div>
-         {textToRender}
-      </div>)
-
-
+      <div className='common'>
+         <div className='text'>
+            { isUppercasetext ? text.toUpperCase() : text }
+         </div>
+         {descriptionSection && descriptionSection }
+      </div>
+     )
 }
 
 export default NavigationItem;
